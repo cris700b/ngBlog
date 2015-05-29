007016815c25b5544c6be525aee2ad61102ca3b1
Cristian Birzu <cristian.birzu@gmail.com>
2015-05-29 19:13:12 +0200

    Added the controllers body for the admin module

--- app/modules/admin/js/admin_ctrl.js
+++ app/modules/admin/js/admin_ctrl.js
@@ -8,19 +8,59 @@
 		
 	    })
 		
-		.controller('PostListCtrl', function($scope){
+		.controller('PostListCtrl', function($scope, $state, PostSrv, PopupSrv){
 			
+			$scope.posts = PopupSrv.query();
+			
+			$scope.deletePost = function(post){
+				
+				if(PopupSrv.showPopup('Really delete this post?')){
+					
+					post.$delete(function(){
+						
+						// once deleted, reload the state
+						$state.go('admin.postList', undefined, {
+							
+							reload: true;
+						});
+					});
+				}
+			};
+		}) 
+		
+		.controller('PostCreationCtrl', function($scope,  $state, PostSrv){
+			
+			$scope.post = new PostSrv();
+			$scope.buttonText = 'Create';
+			
+			$scope.savePost = function(){
+				
+				$scope.buttonText = 'Saving ...';
+			};
+			
+			$scope.post.permalink = angular.lowercase($scope.post.title).replace(/[\s]/g, '-');
+			
+			$scope,post.$save(function(){
+				
+				$state.go('admin.postList');
+			});			
 			
 		}) 
 		
-		.controller('PostCreationCtrl', function($scope){
+		.controller('PostUpdateCtrl', function($scope, $stateParams, $state, PostSrv){
 			
+			$scope.post = PostSrv.get({id: $stateParams.id});
+			$scope.buttonText = 'Update';
 			
-		}) 
-		
-		.controller('PostUpdateCtrl', function($scope){
+			$scope.updatePost = function(){
+				
+				$scope.buttonText = 'Updating ...';
+			};
 			
-			
+			$state.post.$update(function(){
+				
+				$state.go('admin.postList');
+			});
 		});
 })
 (angular.module('admin.controllers', [
